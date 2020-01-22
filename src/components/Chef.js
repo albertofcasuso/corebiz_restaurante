b6aae807263246b07e3cecfe8678dcaa57103334
casutypd <hi@casuso.dev>
2020-01-22 11:01:48 -0600

    chef

--- src/components/Chef.js
+++ src/components/Chef.js
@@ -1,28 +1,32 @@
-import React from 'react'
+import React from "react"
 import chefImg from "../images/chef.jpg"
+import chefhat from "../images/chefhat.png"
 
-const Chef = () =>{
-    return(
-        <section id="ourChef">
-        <div className="container">
-          <div className="row align-items-center mb-5">
-            <div className="col-lg-5 col-12 offset-lg-1 py-5">
-              <img src={chefImg} className="img-fluid" />
+const Chef = () => {
+  return (
+    <section id="ourChef">
+      <div className="container">
+        <div className="row align-items-center justify-content-center my-5">
+          <div className="col-lg-5 col-12 offset-lg-1 py-5">
+            <div class="square p-lg-5 shadow align-items-center p-4">
+              <img src={chefhat} className="mt-lg-3 ml-lg-3" />
             </div>
-            <div className="col-lg-5  col-12 offset-lg-1 float-right">
-              <h6>The word from</h6>
-              <h1>Our Chef</h1>
-              <p>
-                A peep at some distant orb has power to raise and purify our
-                thoughts like a strain of sacred music, or a noble picture, or a
-                passage from the grander poets. It always does one good.
-              </p>
-              <p>Gordon Ramsey</p>
-              <small>Head Chef</small>
-            </div>
+            <img src={chefImg} className="img-fluid" />
+          </div>
+          <div className="col-lg-5  col-12 offset-lg-1 float-right">
+            <p className="lead font-weight-light">The word from</p>
+            <h1 className="display-4">Our Chef</h1>
+            <p className="font-weight-light">
+              A peep at some distant orb has power to raise and purify our
+              thoughts like a strain of sacred music, or a noble picture, or a
+              passage from the grander poets. It always does one good.
+            </p>
+            <p className="lead font-weight-bold mt-4">Gordon Ramsey</p>
+            <p className="text-secondary font-weight-light">Head Chef</p>
           </div>
         </div>
-      </section>
-    )
+      </div>
+    </section>
+  )
 }
 export default Chef
