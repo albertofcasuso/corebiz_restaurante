2e3b8b05950695a6691f6bfd5e31f7c29302358f
casutypd <hi@casuso.dev>
2020-01-22 01:26:04 -0600

    bootstrap

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -29,21 +29,13 @@ const Layout = ({ children }) => {
           href="https://fonts.googleapis.com/css?family=PT+Serif|Roboto&display=swap"
           rel="stylesheet"
         />
-        <script
-          src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-          integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-          crossorigin="anonymous"
-        ></script>
+        <script src={"/jquery-3.4.1.min.js"}></script>
         <script
           src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
           integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
           crossorigin="anonymous"
         ></script>
-        <script
-          src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-          integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-          crossorigin="anonymous"
-        ></script>
+        <script src={"/bootstrap-4.4.1-dist/js/bootstrap.min.js"}></script>
         <script
           src="https://kit.fontawesome.com/7c87fa191a.js"
           crossorigin="anonymous"
@@ -52,7 +44,6 @@ const Layout = ({ children }) => {
       <div>
         <main>{children}</main>
         <Footer />
-        
       </div>
     </>
   )
